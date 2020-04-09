9bbc759569d0884b797ae81f333ff3761f844063
mega <Mega@program.com.tw>
2020-04-09 11:58:08 +0800

    first

---
+++ app.js
@@ -0,0 +1,44 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '/views/build')))
+app.use('/api', indexRouter);
+
+// Anything that doesn't match the above, send back index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/build/index.html'))
+})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('黑人問號!');
+});
+
+module.exports = app;
